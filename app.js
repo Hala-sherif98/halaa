967c721ef7e3826cffde182bf9798adad2079b0c
Hala-sherif98 <halasherif714@gmail.com>
2023-02-04 10:17:49 +0200

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json())
+app.get ('/', (req,res,next)=>{
+
+    res.status(200).send('hellooooooooooo world')
+})
+
+app.listen(port,()=>{
+
+    console.log(`this app is listening on port${port}`)
+})
